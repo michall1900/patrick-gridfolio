3ed2a066d3189965feaef9824bda6ed5d8b389c7
Michal Bucks <michall1900@gmail.com>
2025-09-30 13:53:33 +0300

    Add the side bar for mobiles, center it by the close icon, and add animation with transition to it. Also fix some of the header size and change the z index to some elements.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
   document.addEventListener('DOMContentLoaded', () => {
     const iconElement = document.getElementById('icon');
+    const mobileNav = document.getElementById('mobile-nav');
     iconElement.addEventListener('click', ()=> {
       iconElement.classList.toggle('playing-animation');
+      mobileNav.classList.toggle('open-mobile-nav');
     })
     
   });
